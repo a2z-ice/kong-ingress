3bad2077fdc9333663c8ab74109cd2dff956ea23
Assaduzzaman <a2z.ice@gmail.com>
2022-04-09 15:08:24 +0530

    added consumer group for acl as consumer credentials

--- kong-dbless/2-jwt-credential-secret.sh
+++ kong-dbless/2-jwt-credential-secret.sh
@@ -3,3 +3,6 @@ kubectl create secret generic assad-jwt  \
   --from-file=rsa_public_key=key.pub  \
   --from-literal=key=jwt-user \
   --from-literal=algorithm=RS256
+
+# Added as consumer group which called acl credential
+kubectl create secret generic api-user-acl --from-literal=kongCredType=acl --from-literal=group=api-user
